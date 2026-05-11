37a3dd68f07184f9e4ccbad758138ae25c774029
luca <22luca08@gmail.com>
2026-05-11 22:15:17 +0200

    toretta completata

--- src/main/java/com/example/demo/Torretta.java
+++ src/main/java/com/example/demo/Torretta.java
@@ -14,7 +14,7 @@ public class Torretta {
     public double y;
     public double danno;
     public double gittata;
-    public long cooldownNs;     // tempo tra un colpo e l altro in nanosecondi
+    public long cooldownSecondi;     // tempo tra un colpo e l altro in nanosecondi
     public long ultimoSparo;    // timestamp dell ultimo sparo
     public int costo;
     public boolean rallenta;    // solo torretta magica
@@ -28,7 +28,27 @@ public class Torretta {
         if (tipo == freccia){
             this.danno = 15;
             this.gittata = 120;
+            this.costo = 50;
+            this.cooldownSecondi = (long) 0.8;
+            this.rallenta = false;
+        }
+        else if (tipo == MAGICA) {
+            this.danno           = 30;
+            this.gittata         = 180;
+            this.cooldownSecondi = (long) 2.0;
+            this.costo           = 100;
+            this.rallenta        = true;
+        }
+        else if (tipo == BOMBA) {
+            this.danno           = 50;
+            this.gittata         = 100;
+            this.cooldownSecondi = (long) 3.5;
+            this.costo           = 150;
+            this.rallenta        = false;
         }
 
+        // Calcolo automatico dei nanosecondi: 1 secondo = 1.000.000.000 ns
+        this.cooldownSecondi = this.cooldownSecondi * 1_000_000_000;
     }
 }
+
